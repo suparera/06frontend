478d071b2412ac2088935ad1a5d8c4f64d7b77a5
Suparerk A <suparera@gmail.com>
2018-12-25 15:06:28 +0700

    add Open Sesame Message

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-          change   <code>src/App.js</code> and save to reload.
+          change Open Sesame <code>src/App.js</code> and save to reload.
           </p>
           <a
             className="App-link"
